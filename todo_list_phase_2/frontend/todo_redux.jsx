2806f1a6004e73c36688cb1f84fcc837a8e89f12
L412 <skylarprill@gmail.com>
2018-12-04 18:23:46 -0800

    Fix variable on window, final commit

--- todo_list_phase_2/frontend/todo_redux.jsx
+++ todo_list_phase_2/frontend/todo_redux.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 
 import configureStore from "./store/store";
 import Root from "./components/root";
+import {fetchTodos} from "./util/todo_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
   window.store = store;
+  debugger
+  window.fetchTodos = fetchTodos;
   ReactDOM.render(<Root store={store} />, document.getElementById("root"));
 });
